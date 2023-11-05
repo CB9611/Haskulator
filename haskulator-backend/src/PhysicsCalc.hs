54e66dc0a09bea24635650389ec1f9383d573bb0
kaiihayes <hayeskaitlyn10@gmail.com>
2023-11-05 13:32:03 -0600

    made final functions for haskulator

--- haskulator-backend/src/PhysicsCalc.hs
+++ haskulator-backend/src/PhysicsCalc.hs
@@ -1,4 +1,23 @@
-module PhysicsCalc where
+module PhysicsCalc (calculateDisplacement, calculateDisplacementWithoutVelocityOrAcceleration, calculateAcceleration, calculateAccelerationForceMass, calculateVelocity, calculateVelocityWithFinal) where
+
+
+-- Calculate displacement based on initial velocity, acceleration, and time
+calculateDisplacement :: Double -> Double -> Double -> Double
+calculateDisplacement initialVelocity acceleration time = initialVelocity * time + 0.5 * acceleration * time * time
+
+-- Calculate displacement based on final and initial position
+calculateDisplacementWithoutVelocityOrAcceleration :: Double -> Double -> Double 
+calculateDisplacementWithoutVelocityOrAcceleration finalPosition initialPosition = finalPosition - initialPosition
+
+
+-- Calculate acceleration based on initial velocity, final velocity, and time
+calculateAcceleration :: Double -> Double -> Double -> Double
+calculateAcceleration initialVelocity finalVelocity time = (finalVelocity - initialVelocity) / time
+
+-- Calculate accelaeration with force and mass
+calculateAccelerationForceMass :: Double -> Double -> Double
+calculateAccelerationForceMass force mass = force / mass
+
 
 -- Calculate velocity based on initial velocity, acceleration, and time
 calculateVelocity :: Double -> Double -> Double -> Double
@@ -7,16 +26,4 @@ calculateVelocity initialVelocity acceleration time = initialVelocity + accelera
 -- Calculate velocity based on final velocity, initial velocity, and time
 calculateVelocityWithFinal :: Double -> Double -> Double -> Double
 calculateVelocityWithFinal finalVelocity initialVelocity time = (finalVelocity - initialVelocity) / time
-
--- Calculate displacement based on initial velocity, acceleration, and time
-calculateDisplacement :: Double -> Double -> Double -> Double
-calculateDisplacement initialVelocity acceleration time = initialVelocity * time + 0.5 * acceleration * time * time
-
--- Calculate acceleration based on initial velocity, final velocity, and time
-calculateAcceleration :: Double -> Double -> Double -> Double
-calculateAcceleration initialVelocity finalVelocity time = (finalVelocity - initialVelocity) / time
-
--- Calculate time based on initial velocity, final velocity, and acceleration
-calculateTime :: Double -> Double -> Double -> Double
-calculateTime initialVelocity finalVelocity acceleration = (finalVelocity - initialVelocity) / acceleration
 
