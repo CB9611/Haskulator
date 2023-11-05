6895037b93f70a9a186ee61dc0711c5a501493d8
kaiihayes <hayeskaitlyn10@gmail.com>
2023-11-05 11:20:21 -0600

    made variables easier to understand

--- haskulator-backend/src/PhysicsCalc.hs
+++ haskulator-backend/src/PhysicsCalc.hs
@@ -2,21 +2,21 @@ module PhysicsCalc where
 
 -- Calculate velocity based on initial velocity, acceleration, and time
 calculateVelocity :: Double -> Double -> Double -> Double
-calculateVelocity v0 a t = v0 + a * t
+calculateVelocity initialVelocity acceleration time = initialVelocity + acceleration * time
 
 -- Calculate velocity based on final velocity, initial velocity, and time
 calculateVelocityWithFinal :: Double -> Double -> Double -> Double
-calculateVelocityWithFinal vf v0 t = (vf - v0) / t
+calculateVelocityWithFinal finalVelocity initialVelocity time = (finalVelocity - initialVelocity) / time
 
 -- Calculate displacement based on initial velocity, acceleration, and time
 calculateDisplacement :: Double -> Double -> Double -> Double
-calculateDisplacement v0 a t = v0 * t + 0.5 * a * t * t
+calculateDisplacement initialVelocity acceleration time = initialVelocity * time + 0.5 * acceleration * time * time
 
 -- Calculate acceleration based on initial velocity, final velocity, and time
 calculateAcceleration :: Double -> Double -> Double -> Double
-calculateAcceleration v0 v t = (v - v0) / t
+calculateAcceleration initialVelocity finalVelocity time = (finalVelocity - initialVelocity) / time
 
 -- Calculate time based on initial velocity, final velocity, and acceleration
 calculateTime :: Double -> Double -> Double -> Double
-calculateTime v0 v a = (v - v0) / a
+calculateTime initialVelocity finalVelocity acceleration = (finalVelocity - initialVelocity) / acceleration
 
