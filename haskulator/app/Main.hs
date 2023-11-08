24edf9fd4a0833348c4072fc82b838ca18bb34bd
Vasylku <vasylkuvayskov@gmail.com>
2023-11-08 01:00:00 -0600

    fetch get post html, post haskell

--- haskulator/app/Main.hs
+++ haskulator/app/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
-import Data.Aeson (ToJSON, object, (.=),toJSON)
-import Data.Text.Lazy (Text, pack)  -- Import Data.Text and use pack to convert strings to Text
+import Data.Aeson (Value, ToJSON, FromJSON, object, (.=), (.:), toJSON, decode, parseJSON, Object)
+import Data.Aeson
+import Data.Text.Lazy  -- Import Data.Text and use pack to convert strings to Text
 import Network.Wai.Middleware.Cors (simpleCors)  
+import Control.Applicative
+import Data.Text.Lazy.Encoding
 
 data PhysicsCategory = PhysicsCategory Text
   deriving (Show)
@@ -16,12 +19,36 @@ physicsCategories =
   [ PhysicsCategory "Displacement"
   , PhysicsCategory "Acceleration"
   , PhysicsCategory "Projectile"
+  , PhysicsCategory "Momentum"
+  , PhysicsCategory "Velocity"
   ]
 
+
+data DisplacementProblem = DisplacementProblem Double Double Double
+    deriving (Show)
+
+instance FromJSON DisplacementProblem where
+    parseJSON (Object v) = DisplacementProblem
+        <$> v .: "initialVelocity"
+        <*> v .: "angle"
+        <*> v .: "acceleration"
+
+
+instance ToJSON DisplacementProblem where
+    toJSON (DisplacementProblem v0 angle a) =
+      object [ "initialVelocity" .= v0
+        , "angle" .= angle
+        , "acceleration" .= a]
 main :: IO ()
 main = scotty 3000 $ do
 
   middleware simpleCors  -- Enable CORS
 
   get "/categories" $ do
-    json physicsCategories
+    Web.Scotty.json physicsCategories
+  
+  post "/calculate/:category" $ do
+    categoryParam <- param "category" :: ActionM Text
+    mydata <- jsonData :: ActionM DisplacementProblem
+    Web.Scotty.json mydata
+
